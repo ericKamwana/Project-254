40388214a1a72c462edb84e5906695d039a8b3f8
ericKamwana <hb.3kungu@gmail.com>
2025-03-04 15:37:15 +0300

    Initial commit: Add diceroll.py

---
+++ diceroll.py
@@ -0,0 +1,6 @@
+import random
+# generate a random number between 1 and 6 (like rolling a dice)
+dice_roll=random.randit(1,10)
+print("Play your turn now", dice_roll)
+
+
---
+++ minichallenge.py
@@ -0,0 +1,22 @@
+# write a python function that checks if a number is even or odd and print the result
+def check_even_odd (number):
+   if number %2 ==0:
+      print(number,"is even!") 
+   else:
+      print(number,"is odd")
+
+temperature = 30
+if temperature > 25:
+   print("its hot you should drink water")
+elif temperature <=25:
+   print("its warm relax")
+elif temperature <=8:
+   print("its cold keep warm")
+elif temperature <=0:
+   print("Critical temperatures")
+else:
+   print("warming")
+   
+
+   
+       
